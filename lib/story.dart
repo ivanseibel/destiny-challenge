99fae25b6b0b78bec7252b60063f08b592a4140d
Ivan Luis Seibel <ivanseibel@gmail.com>
2020-01-17 19:07:57 -0300

    Initial commit

---
+++ lib/story.dart
@@ -0,0 +1,14 @@
+class Story {
+  String storyTitle;
+  String choice1Text;
+  String choice2Text;
+  int choice1NextStory;
+  int choice2NextStory;
+
+  Story(
+      {this.storyTitle,
+      this.choice1Text,
+      this.choice2Text,
+      this.choice1NextStory,
+      this.choice2NextStory});
+}
